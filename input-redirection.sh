deb60b05274ea8f1661729bfc625bfe04ef1b3d4
hashem <hashemalsaie0457@gmail.com>
2026-06-30 00:46:48 +0300

    pushed

---
+++ input-redirection.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat > show-info.sh << 'SCRIPT'
+#!/bin/bash
+
+cat -e << INFO
+The current directory is: $PWD
+The default paths are: $PATH
+The current user is: $USERNAME
+INFO
+SCRIPT
