8d130176321369b9babf986a6c4b31514cee296a
hashem <hashemalsaie0457@gmail.com>
2026-06-30 00:59:15 +0300

    pushed

--- array-selector.sh
+++ array-selector.sh
@@ -3,21 +3,18 @@
 colors=("red" "blue" "green" "white" "black")
 
 if [[ $# -ne 1 ]]; then
-  printf "Error\n"
+  echo "Error"
   exit 1
 fi
 
 if [[ ! "$1" =~ ^[0-9]+$ ]]; then
-  printf "Error\n"
+  echo "Error"
   exit 1
 fi
 
-position=$((10#$1))
-
-if [[ $position -lt 1 || $position -gt ${#colors[@]} ]]; then
-  printf "Error\n"
+if [[ "$1" -lt 1 || "$1" -gt 5 ]]; then
+  echo "Error"
   exit 1
 fi
 
-index=$((position - 1))
-printf "%s\n" "${colors[$index]}"
+echo "${colors[$1-1]}"
