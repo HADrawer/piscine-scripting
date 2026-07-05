6a55649d69845e3130eff98739c3a7a16f25f7aa
hashem <hashemalsaie0457@gmail.com>
2026-07-05 09:38:49 +0300

    pushed

---
+++ string_tokenizer_count.py
@@ -0,0 +1,12 @@
+import re
+from collections import Counter
+
+
+def tokenizer_counter(string):
+    string = string.lower()
+    string = re.sub(r"[^a-z0-9]+", " ", string)
+
+    words = string.split()
+    counts = Counter(words)
+
+    return dict(sorted(counts.items()))
