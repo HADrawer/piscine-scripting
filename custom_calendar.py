b5264d5c8b240c338da00af768c432189de83957
hashem <hashemalsaie0457@gmail.com>
2026-07-05 09:30:17 +0300

    pushed

---
+++ custom_calendar.py
@@ -0,0 +1,21 @@
+days = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday",
+}
+
+
+def day_from_number(day_number):
+    return days.get(day_number)
+
+
+def day_to_number(day):
+    for number, day_name in days.items():
+        if day_name == day:
+            return number
+
+    return None
